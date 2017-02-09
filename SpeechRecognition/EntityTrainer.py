dd63240229d50820b4a5fd0956486f6bc75dc566
Aaron Karp <aaronkarp123@gmail.com>
2017-02-08 22:07:35 -0600

    Fixed import issues. Added custom entity recognition and support for multi-phrase sentences.

---
+++ SpeechRecognition/EntityTrainer.py
@@ -0,0 +1,38 @@
+import spacy
+
+print("   Loading Spacy...")
+nlp = spacy.load('en')
+print ("   Spacy Loaded")
+
+matcher = spacy.matcher.Matcher(nlp.vocab)
+
+def merge_phrases(matcher, doc, i, matches):
+    """
+    Merge a phrase. We have to be careful here because we'll change the token indices.
+    To avoid problems, merge all the phrases once we're called on the last match.
+    """
+    if i != len(matches) - 1:
+        return None
+    spans = [(ent_id, label, doc[start: end]) for ent_id, label, start, end in matches]
+    for ent_id, label, span in spans:
+        span.merge(u'NNP' if label else span.root.tag_, span.text, nlp.vocab.strings[label])
+
+
+def train():
+    with open('custom_dict_files/Shapes.txt') as f:
+        content = f.readlines()
+    # you may also want to remove whitespace characters like `\n` at the end of each line
+    content = [x.strip() for x in content]
+
+    key = 1
+    for line in content:
+        matcher.add(entity_key=str(key), label='SHAPE', attrs={}, specs=[[{spacy.attrs.ORTH: line.decode('utf-8')}]], on_match=merge_phrases)
+        key += 1
+
+
+def match(doc):
+    matcher(doc)
+    ets = []
+    for ent in doc.ents:
+        ets.append(ent)
+    return ets
--- SpeechRecognition/Interpreter.py
+++ SpeechRecognition/Interpreter.py
@@ -1,85 +1,80 @@
-import nltk
-import spacy
-from spacy.symbols import *
+print('Loading imports...')
 
-nlp = spacy.load('en')
+import EntityTrainer as Et
+from EntityTrainer import nlp
 
+print('Imports loaded')
+print
 
-def parse_phrase_deprecated(phrase):
-	tokens = nltk.word_tokenize(phrase)
-	tagged = nltk.pos_tag(tokens)
-	print 'Tokenized phrase: ', tagged
-	print
-	find_objects_deprecated(tagged)
-
-
-def find_objects_deprecated(tagged):
-	print 'Just for testing purposes...'
-	print 'NN: ',
-	for word in tagged:
-		if word[1][:2] == 'NN':
-			print word[0], ' ',
-	print
-	print
+Et.train()
 
 
 def text2int(textnum, numwords={}):
-	if not numwords:
-		units = [
-			"zero", "one", "two", "three", "four", "five", "six", "seven", "eight",
-			"nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen",
-			"sixteen", "seventeen", "eighteen", "nineteen",
-		]
+    if not numwords:
+        units = [
+            "zero", "one", "two", "three", "four", "five", "six", "seven", "eight",
+            "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen",
+            "sixteen", "seventeen", "eighteen", "nineteen",
+        ]
 
-		tens = ["", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
+        tens = ["", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
 
-		scales = ["hundred", "thousand", "million", "billion", "trillion"]
+        scales = ["hundred", "thousand", "million", "billion", "trillion"]
 
-		numwords["and"] = (1, 0)
-		for idx, word in enumerate(units):    numwords[word] = (1, idx)
-		for idx, word in enumerate(tens):     numwords[word] = (1, idx * 10)
-		for idx, word in enumerate(scales):   numwords[word] = (10 ** (idx * 3 or 2), 0)
+        numwords["and"] = (1, 0)
+        for idx, word in enumerate(units):    numwords[word] = (1, idx)
+        for idx, word in enumerate(tens):     numwords[word] = (1, idx * 10)
+        for idx, word in enumerate(scales):   numwords[word] = (10 ** (idx * 3 or 2), 0)
 
-	current = result = 0
-	for word in textnum.split():
-		if word not in numwords:
-			raise Exception("Illegal word: " + word)
+    current = result = 0
+    for word in textnum.split():
+        if word not in numwords:
+            raise Exception("Illegal word: " + word)
 
-		scale, increment = numwords[word]
-		current = current * scale + increment
-		if scale > 100:
-			result += current
-			current = 0
+        scale, increment = numwords[word]
+        current = current * scale + increment
+        if scale > 100:
+            result += current
+            current = 0
 
-	return result + current
+    return result + current
 
 
 def parse_phrase(phrase):
-	doc = nlp(unicode(phrase, encoding="utf-8"))
-	print doc
-	objects = []
-	verbs = []
-	quantity = []
-	unit = []
-	for np in doc.noun_chunks:
-		objects.append(np.root.text)
-		verbs.append(np.root.head.text)
-		print np.text, np.root.text, np.root.dep_, np.root.head.text
-	for word in doc:
-		if word.pos == NOUN and word.head.pos == VERB:
-			if word.text not in objects:
-				unit.append(word)
-				quantity.append(list(word.lefts)[0])
-	print 'verbs: ', verbs
-	print 'objects: ', objects
-	print 'quanitity: ', quantity
-	print 'unit: ', unit
-	for i in range(len(verbs)):
-		take_action(verbs[i], objects[i], quantity[i], unit[i])
+    doc = nlp(unicode(phrase, encoding="utf-8"))
+    print ("Decoding : ", doc)
+    objects = []
+    verbs = []
+    quantity = []
+    unit = []
+    # Idea for adjective chaining...
+    # First find shape
+    # Look for instance of shape name in all NN's
+    # Find adjectives within that
+    for ent in Et.match(doc):
+        print(ent.label_, ent.text)
+        if ent.label_ == u'QUANTITY':
+            quantity.append(ent.text.lower())
+        elif ent.label_ == u'SHAPE':
+            objects.append(ent.text.lower())
+    for np in doc.noun_chunks:
+        if np.root.tag_ == 'PRP':
+            if len(objects) > 0:
+                objects.append(''.join(objects[-1:]))
+            else:
+                print ('ERROR: Unable to find object of prepositional phrase.')
+                return
+        if np.root.head.tag_ != 'IN':
+            verbs.append(np.root.head.text.lower())
+    print 'verbs: ', verbs
+    print 'objects: ', objects
+    print 'quantity: ', quantity
+    for i in range(len(verbs)):
+        take_action(verbs[i], objects[i], quantity[i])
 
 
-def take_action(verb, object, quantity, unit):
-	print 'Calling ', verb, ' with arguments: ', object, ', ', text2int(quantity.text), unit
+def take_action(verb, obj, quantity):
+    print 'Calling ', verb, ' with arguments: ', obj, ', ', text2int(''.join(quantity.split(" ")[:-1])), ''.join(quantity.split(" ")[-1:])
 
 
-parse_phrase('Move the circle down fifty pixels.')
+parse_phrase('Move the circle down fifty pixels, then move it down by twenty pixels.')
