9cb7a83d7ffb6a70661d0c7cc148673e384f2ab5
Aaron Karp <aaronkarp123@gmail.com>
2017-02-09 10:32:21 -0600

    Added adjective chaining.

--- SpeechRecognition/Interpreter.py
+++ SpeechRecognition/Interpreter.py
@@ -44,6 +44,7 @@ def parse_phrase(phrase):
     doc = nlp(unicode(phrase, encoding="utf-8"))
     print ("Decoding : ", doc)
     objects = []
+    adjectives = []
     verbs = []
     quantity = []
     unit = []
@@ -58,23 +59,36 @@ def parse_phrase(phrase):
         elif ent.label_ == u'SHAPE':
             objects.append(ent.text.lower())
     for np in doc.noun_chunks:
+        found_shape = False
         if np.root.tag_ == 'PRP':
-            if len(objects) > 0:
+            if len(objects) > 0:  # If the preposition is referring to a previous object...
                 objects.append(''.join(objects[-1:]))
+                adjectives.append(adjectives[-1:][0])
             else:
                 print ('ERROR: Unable to find object of prepositional phrase.')
                 return
+        for word in np:
+            if word.text.lower() in objects:
+                found_shape = True
+        if found_shape:
+            temp_list = list()
+            for word in np:
+                if word.pos_ == 'ADJ':
+                    temp_list.append(word.text.lower())
+            adjectives.append(temp_list)
+
         if np.root.head.tag_ != 'IN':
             verbs.append(np.root.head.text.lower())
     print 'verbs: ', verbs
     print 'objects: ', objects
+    print 'descriptors: ', adjectives
     print 'quantity: ', quantity
     for i in range(len(verbs)):
-        take_action(verbs[i], objects[i], quantity[i])
+        take_action(verbs[i], objects[i], adjectives[i], quantity[i])
 
 
-def take_action(verb, obj, quantity):
-    print 'Calling ', verb, ' with arguments: ', obj, ', ', text2int(''.join(quantity.split(" ")[:-1])), ''.join(quantity.split(" ")[-1:])
+def take_action(verb, obj, desc, quantity):
+    print 'Calling ', verb, ' with arguments: ', ' '.join(desc), obj, ', ', text2int(''.join(quantity.split(" ")[:-1])), ''.join(quantity.split(" ")[-1:])
 
 
-parse_phrase('Move the circle down fifty pixels, then move it down by twenty pixels.')
+parse_phrase('Move the small blue circle down fifty pixels, then move it down by twenty pixels.')
