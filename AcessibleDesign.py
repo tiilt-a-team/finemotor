90671bbbc0d00641d7a1c1a125f8d3072adcc99c
Aaron Karp <aaronkarp123@gmail.com>
2017-05-18 18:19:30 -0500

    Single execute file. Run AcessibleDesign.py and all other processes (excluding Blender currently) will be run

---
+++ AcessibleDesign.py
@@ -0,0 +1,64 @@
+import os
+import signal
+import subprocess
+import time
+import sys
+
+global subs
+
+def main():
+	global subs
+	subs = []
+
+	sys.stdout.write("\033[1;31m") # Red
+	print 'Press Ctrl-C to kill all AccessibleDesign processes'
+
+	try:
+		eye_tribe = subprocess.Popen("EyeTracker/EyeTribe", stdout=subprocess.PIPE, shell=False, preexec_fn=os.setsid)
+		subs.append(eye_tribe)
+		#time.sleep(1)
+		recognition_sockets = subprocess.Popen(['python', 'RecognitionSockets.py'], cwd='SpeechRecognition', stdout=subprocess.PIPE, shell=False, preexec_fn=os.setsid)
+		subs.append(recognition_sockets)
+		#time.sleep(1)
+
+		sys.stdout.write("\033[0;32m") # Green
+		print ''
+		print 'Process PIDs...'
+		print 'eye_tribe = ', eye_tribe.pid
+		print 'recognition_sockets = ', recognition_sockets.pid
+		sys.stdout.write("\033[0;0m") # Reset
+		print ''
+
+	except Exception as e:
+		# If any of them couldn't open
+		raise e
+		exit()
+
+	while True:
+		try:
+			for sub in subs:
+				if sub.poll():
+					exit()
+		except KeyboardInterrupt:
+			exit()
+
+
+# Kill processes and exit shell
+def exit():
+	global subs
+	sys.stdout.write("\033[1;31m") # Red
+	for sub in subs:
+		try:
+			os.killpg(os.getpgid(sub.pid), signal.SIGTERM)
+		except Exception as e:
+			print ''
+			print 'Subprocess error'
+			print e, ' -- ', sub.pid
+	sys.stdout.write("\033[0;0m") # Reset
+	print ''
+	sys.exit()
+
+
+
+
+main()
