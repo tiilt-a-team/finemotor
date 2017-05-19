03fb4e9eb72ed0d0643dbdbe56834868f78dbab0
Aaron Karp <aaronkarp123@gmail.com>
2017-05-19 10:33:40 -0500

    Added functionality for Blender.

--- AcessibleDesign.py
+++ AcessibleDesign.py
@@ -4,12 +4,18 @@ import subprocess
 import time
 import sys
 import platform
+from Tkinter import *
+import tkFileDialog
 
 global subs
+global directory
 
 def main():
 	global subs
+	global directory
+
 	subs = []
+	directory = None
 
 	sys.stdout.write("\033[1;31m") # Red
 	print 'Press Ctrl-C to kill all AccessibleDesign processes'
@@ -17,6 +23,15 @@ def main():
 	plat = platform
 
 	try:
+		root = Tk()
+		root.withdraw()
+		root.update()
+		directory = tkFileDialog.askdirectory(initialdir='.')
+		while not directory:
+			time.sleep(0.1)
+		root.update()
+		root.destroy()
+
 		if plat.system() == 'Windows': # Windows
 			eye_tribe = subprocess.Popen("EyeTracker/EyeTribe.exe", stdout=subprocess.PIPE, shell=False, preexec_fn=os.setsid)
 		elif plat.system() == 'Darwin': # MacOS
@@ -24,17 +39,21 @@ def main():
 		else:
 			print 'Unsupported Operating System'
 			exit()
+		
 		subs.append(eye_tribe)
-		#time.sleep(1)
 		recognition_sockets = subprocess.Popen(['python', 'RecognitionSockets.py'], cwd='SpeechRecognition', stdout=subprocess.PIPE, shell=False, preexec_fn=os.setsid)
 		subs.append(recognition_sockets)
-		#time.sleep(1)
+
+		print directory
+		blender = subprocess.Popen('./blender.app/Contents/MacOS/blender -d --python tiilt/blender.py', cwd=directory, shell=True, preexec_fn=os.setsid)
+		subs.append(blender)
 
 		sys.stdout.write("\033[0;32m") # Green
 		print ''
 		print 'Process PIDs...'
 		print 'eye_tribe = ', eye_tribe.pid
 		print 'recognition_sockets = ', recognition_sockets.pid
+		print 'blender = ', blender.pid
 		sys.stdout.write("\033[0;0m") # Reset
 		print ''
 
@@ -51,6 +70,10 @@ def main():
 		except KeyboardInterrupt:
 			exit()
 
+
+def openDir():
+    global directory
+    directory = tkFileDialog.askdirectory(initialdir='.')
 
 # Kill processes and exit shell
 def exit():
